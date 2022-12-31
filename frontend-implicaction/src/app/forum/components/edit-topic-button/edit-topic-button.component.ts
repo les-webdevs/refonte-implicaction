d184ec87a62961ba4e6c5fd8f059d73e2159f3b0
cosiewicz <christophe.osiewicz@gmail.com>
2022-12-31 16:23:43 +0100

    init form & button components created

---
+++ frontend-implicaction/src/app/forum/components/edit-topic-button/edit-topic-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-topic-button',
+  templateUrl: './edit-topic-button.component.html',
+  styleUrls: ['./edit-topic-button.component.scss']
+})
+export class EditTopicButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend-implicaction/src/app/forum/components/edit-topic-form/edit-topic-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-topic-form',
+  templateUrl: './edit-topic-form.component.html',
+  styleUrls: ['./edit-topic-form.component.scss']
+})
+export class EditTopicFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
