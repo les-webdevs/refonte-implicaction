316b5c9a1e96ec9d0bec264a5e19fd2795840a43
cosiewicz <christophe.osiewicz@gmail.com>
2023-01-10 14:44:27 +0100

    add admin forum component, add admin forum routing

--- frontend-implicaction/src/app/admin/forum/forum.module.ts
+++ frontend-implicaction/src/app/admin/forum/forum.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ForumComponent} from './forum.component';
+import {ForumRoutingModule} from './forum-routing.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ForumComponent
+  ],
   imports: [
+    ForumRoutingModule,
     CommonModule
   ]
 })
-export class ForumModule { }
+export class ForumModule {
+}
