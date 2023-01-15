d3924ebb74287dace953ebce54053fe662af7836
Vahelnir <vahelnir@gmail.com>
2023-01-15 17:45:15 +0100

    get category with updated topic

--- frontend-implicaction/src/app/forum/components/category-content/category-content.component.ts
+++ frontend-implicaction/src/app/forum/components/category-content/category-content.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {CategoryService} from "../../services/category.service";
 import {ActivatedRoute} from "@angular/router";
 import {map, switchMap} from "rxjs/operators";
-import {Observable} from "rxjs";
+import {Observable, of} from "rxjs";
 import {Category} from "../../model/category";
 import {
   BaseWithPaginationAndFilterComponent
@@ -34,7 +34,11 @@ export class CategoryContentComponent extends BaseWithPaginationAndFilterCompone
     // TODO: demander à Matthieu s'il y a une meilleure façon de gerer ça.
     // Actuellement, faire un `subCategories$ | async` fait un refetch du
     // `category$` ce qui n'est pas vraiment une bonne chose. (pas envie de passer par un subscribe)
-    this.subCategories$ = this.category$.pipe(switchMap(category => this.categoryService.getCategories(category.children)));
+    this.subCategories$ = this.category$.pipe(
+      switchMap(category => category.children.length > 0
+        ? this.categoryService.getCategories(category.children, {withRecentlyUpdatedTopic: true})
+        : of(null))
+    );
     this.paginate(this.pageable);
   }
 
--- frontend-implicaction/src/app/forum/model/category.ts
+++ frontend-implicaction/src/app/forum/model/category.ts
@@ -1,7 +1,10 @@
+import {Topic} from './topic';
+
 export interface Category {
   id: number;
   title: string;
   description?: string;
   parentId?: number;
   children: number[];
+  recentlyUpdatedTopic?: Topic;
 }
